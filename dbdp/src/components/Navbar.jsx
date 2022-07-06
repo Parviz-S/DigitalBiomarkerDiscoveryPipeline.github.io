6dbf8b88d5b14fb66199afe840a46495071674ca
Parviz Subkhankulov <ps310@duke.edu>
2022-07-06 12:37:50 +0500

    init

---
+++ dbdp/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[90px] bg-black">
+        <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
+            <div>
+                <h1 className="text-[#00d8ff]">dbdp</h1>
+            </div>
+            <div>
+                <ul className="flex text-white">
+                    <li>Github</li>
+                    <li>Twitter</li>
+                    <li>Blog</li>
+                    <li>dbdpED</li>
+                    <button>Open dbdp</button>  
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
