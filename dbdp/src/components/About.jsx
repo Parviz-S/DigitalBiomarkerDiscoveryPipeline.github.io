9086cfa77228dd4ca400e32636e31c770ed0231f
Parviz Subkhankulov <ps310@duke.edu>
2022-07-07 22:44:38 +0500

    About cards

---
+++ dbdp/src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { TbSection } from 'react-icons/tb'
+import { BsGrid3X3GapFill } from 'react-icons/bs'
+import { GiEarthAmerica } from 'react-icons/gi'
+
+import AboutCard from './AboutCard'
+
+const About = () => {
+  return (
+    <div className="bg-black text-white text-center">
+        <div className="max-w-[1240px] mx-auto px-4 py-16 ">
+            <h1 className="py-4">What is DBDP?</h1>
+            <p1 className="py-4 text-xl">The Digital Biomarker Discovery Pipeline (DBDP) is an open source platform for the development of digital biomarkers.</p1>
+
+            {/* Card container */}
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+
+                {/* card */}
+                <AboutCard icon={<BsGrid3X3GapFill size={40}/>} heading={"End-to-End"} text={"The DBDP provides complete end-to-end digital biomarker development. Modules are dedicated to data pre-processing, data analysis, algorithm development, and validation."} />
+                <AboutCard icon={<TbSection size={40}/>} heading={"Modular Framework"} text={"The DBDP is a modular framework, providing both end-to-end complete solutions as well as tools and algorithms for discovery."} />
+                <AboutCard icon={<GiEarthAmerica size={40}/>} heading={"Open Source"} text={"The DBDP is open source under the Apache 2.0 License. The DBDP is available in GitHub under a Contributor Code of Conduct."} />
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ dbdp/src/components/AboutCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutCard = (props) => {
+  return (
+    <div  className="flex flex-col border rounded-2xl text-left py-12 px-8">
+        <div>
+            <div className={`bg-[#00d8ff] inline-flex p-2 rounded-full`}>{props.icon}</div>
+            <h3 className="text-2xl font-bold py-4">{props.heading}</h3>
+            <p>{props.text}</p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutCard
