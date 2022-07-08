a6fe83dadf8620b7dddf599a961e2eb21f0c84f2
Parviz Subkhankulov <ps310@duke.edu>
2022-07-08 23:41:13 +0500

    Add card animation on hover

--- dbdp/src/components/AboutCard.jsx
+++ dbdp/src/components/AboutCard.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const AboutCard = (props) => {
   return (
-    <div  className="flex flex-col border rounded-2xl text-left py-12 px-8">
-        <div>
-            <div className={`bg-[#00d8ff] inline-flex p-2 rounded-full`}>{props.icon}</div>
-            <h3 className="text-2xl font-bold py-4">{props.heading}</h3>
-            <p>{props.text}</p>
+    <div className="cursor-pointer">
+        <div class="inset-0 transform hover:scale-95 transition duration-300">
+            <div  className="flex flex-col border rounded-2xl text-left py-12 px-8">
+                <div>
+                    <div className={`bg-[#00d8ff] inline-flex p-2 rounded-full`}>{props.icon}</div>
+                    <h3 className="text-2xl font-bold py-4">{props.heading}</h3>
+                    <p>{props.text}</p>
+                </div>
+            </div>
         </div>
     </div>
+    
   )
 }
 
