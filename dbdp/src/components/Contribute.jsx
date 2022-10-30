4abbe562bfab3008f324b291b293d9e648824cdd
Parviz Subkhankulov <ps310@duke.edu>
2022-10-30 12:01:29 +0500

    contribute page updates on text and margins

--- dbdp/src/components/Contribute.jsx
+++ dbdp/src/components/Contribute.jsx
@@ -4,14 +4,14 @@ const Contribute = () => {
   return (
     <div className='w-full text-white bg-black'>
       <div className='grid grid-cols-2 gap-8 pt-[10%] pb-[5%] pl-10 pr-10 w-[90%] mx-auto'>
-        <div className='text-center'>
+        <div className='text-center pr-6'>
           <h1 className='mb-10'>Welcome!</h1>
           <p className='mt-5 text-xl text-left'>It’s simple to contribute your efforts on digital biomarker discovery (e.g., codes, datasets) to the DBDP.</p>
-          <div className='ml-[25%]'><p className='mt-5 text-left'>1. Read through instructions.</p>
+          <div className='ml-[25%]'><p className='mt-5 text-xl text-left'>1. Read through instructions.</p>
           <p className='mt-2 text-xl text-left'>2. Submit form.</p>
           <p className='mt-2 text-xl italic text-left'>Done!</p></div>
         </div>
-        <div className='text-center'>
+        <div className='text-center pl-6'>
           <h1 className='mb-10'>Tell us about your code</h1>
           <p className='mt-5 text-xl text-justify'>Click ‘Complete Form’ to be redirected to a separate window to answer a few questions to share your code with DBDP.</p> 
           <p className='mt-5 text-xl text-justify'>It should take a total of ~15 minutes to complete, and we will reach out to you as soon as we review the submitted form.</p>
