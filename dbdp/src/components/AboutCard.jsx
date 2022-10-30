2ea76d70eabdf4d6e2a34de7cad4e478976d1942
Parviz Subkhankulov <ps310@duke.edu>
2022-10-30 11:54:23 +0500

    aligning about card components to have the same high

--- dbdp/src/components/AboutCard.jsx
+++ dbdp/src/components/AboutCard.jsx
@@ -4,7 +4,7 @@ const AboutCard = (props) => {
   return (
     <div className="cursor-pointer">
         <div className="inset-0 transform hover:scale-95 transition duration-300">
-            <div  className="flex flex-col border rounded-2xl text-left py-12 px-8">
+            <div  className="flex flex-col border rounded-2xl text-left py-8 px-8 h-[300px]">
                 <div>
                     <div className={`bg-[#00d8ff] inline-flex p-2 rounded-full`}>{props.icon}</div>
                     <h3 className="text-2xl font-bold py-4">{props.heading}</h3>
